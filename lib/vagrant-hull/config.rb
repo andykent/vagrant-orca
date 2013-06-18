fcb9b508fb134c1acd43dd66939fab2133009990
Andy Kent <andy.kent@me.com>
2013-06-18 22:50:45 +0100

    better config defaults

--- lib/vagrant-hull/config.rb
+++ lib/vagrant-hull/config.rb
@@ -1,8 +1,18 @@
 module VagrantPlugins
   module Hull
     class Config < Vagrant.plugin("2", :config)
-      attr_reader :file
-      attr_reader :package
+      attr_accessor :file
+      attr_accessor :package
+
+      def initialize
+        @file = UNSET_VALUE
+        @package = UNSET_VALUE
+      end
+
+      def finalize!
+        @file = './hull/hull.rb' if @file == UNSET_VALUE
+        @package = 'app' if @package == UNSET_VALUE
+      end
     end
   end
 end
--- lib/vagrant-hull/provisioner.rb
+++ lib/vagrant-hull/provisioner.rb
@@ -3,13 +3,17 @@ require "hull"
 module VagrantPlugins
   module Hull
     class Provisioner < Vagrant.plugin(2, :provisioner)
-      include ::Hull::DSL
-
       def provision
         o = @machine.ssh_info
-        require @config.file
-        ::Hull::Node.new(@machine.name, o[:host], :port => o[:port], :user => o[:username], :keys => o[:private_key_path])
-        ::Hull::DSL.execute(@machine.name, @config.package || 'app', :apply)
+        hull_file = File.expand_path(@config.file, @machine.env.root_path)
+        ENV['HULL_FILE'] = hull_file
+
+        suite = ::Hull::Suite.new
+        suite.load_file(hull_file)
+
+        node = ::Hull::Node.new(@machine.name, o[:host], :port => o[:port], :user => o[:username], :keys => o[:private_key_path])
+
+        suite.execute(node, package, :apply)
       end
     end
   end
